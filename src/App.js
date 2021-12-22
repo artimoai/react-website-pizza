e545039d1b6c89a3e002e57a0f4298729f02c563
Andrei <artimoai@users.noreply.github.com>
2021-12-22 13:43:36 +0000

    Fifth commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-import Navbar from './components/navbar.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
+import Home from "./pages/Home.js";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>TEST</h1>
-      <Navbar />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" exact element={<Home />} />
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import pizzaBackground from "../assets/pizzaBackground.jpeg";
+import "../styles/Home.css";
+
+function home() {
+	return (
+		<div
+			className="home"
+			style={{ backgroundImage: `url(${pizzaBackground})` }}
+		>
+			<div className="headerContainer">
+				<h1>Scottish Pizzeria</h1>
+				<p>Pizza. Whenever you're hungry.</p>
+				<Link to="/menu">
+					<button>ORDER NOW</button>
+				</Link>
+			</div>
+		</div>
+	);
+}
+
+export default home;
