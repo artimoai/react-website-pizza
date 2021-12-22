488cdc57c2c73a69a246931ac6add1b61faa0c8f
Andrei <artimoai@users.noreply.github.com>
2021-12-22 03:07:34 +0000

    Second commit

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function navbar() {
+    return (
+        <div className='navbar'>
+            <div className='left-side'>
+                L
+            </div>
+            <div className='right-side'>
+                R
+            </div>
+            
+        </div>
+    )
+}
+
+export default navbar;
